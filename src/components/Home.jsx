7a3c669bc7955d9264ee821146ec7efca6eb288c
ZyongShen <legorye42@gmail.com>
2024-10-18 01:59:51 -0400

    Update intro

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -9,7 +9,7 @@ function Home() {
             <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row">
                 <div className="flex flex-col justify-center h-full">
                     <h2 className="text-4xl sm:text-7xl font-bold text-white">
-                        I'm a Software Engineer
+                        Hi! My name is WingSheng
                     </h2>
                     <p className="text-gray-500 py-4 max-w-md">
                         I'm a software engineer with 2 years of experience working in Angular, Spring-Boot, and AWS
